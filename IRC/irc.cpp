18e6d0b980546a766b2d51bfb8fd300ea9309ed7
younesmoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-25 20:25:45 +0100

    commit with gacp

--- IRC/irc.cpp
+++ IRC/irc.cpp
@@ -9,13 +9,13 @@ void    signal_handler(int signal)
 
 int main(int argc, char **argv)
 {
-    if (argc != 4)
+    if (argc != 3)
     {
         std::cerr << "Error : The program started like this ./ircserv port password !" << std::endl;
         return (EXIT_FAILURE);
     }
 	signal(SIGINT, signal_handler);
-    
+
 
 
     return (EXIT_SUCCESS);
