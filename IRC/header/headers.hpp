ab57ba21056229792f42bfb6a9ea8cf0d3c94ee4
younesmoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-26 17:55:33 +0100

    commit with gacp

--- IRC/header/headers.hpp
+++ IRC/header/headers.hpp
@@ -2,11 +2,22 @@
 
 #pragma once
 
-#include <csignal>
-#include <fstream>
-#include <signal.h>
-#include <iostream>
-#include <socket>
+# include <sys/types.h>
+# include <sys/socket.h>
+# include <netinet/in.h>
+# include <arpa/inet.h>
+# include <netdb.h>
+# include <cstring>
+# include <cstdlib>
+# include <iostream>
+# include <vector>
+# include <map>
+# include <poll.h>
+# include <unistd.h>
+# include <sstream>
+# include <string>
+# include <cerrno>
+# include <ctime>
 
 
 
