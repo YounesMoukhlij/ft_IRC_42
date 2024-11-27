505682ca56eabd90b45b192fdc7a192744f69338
younesmoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-27 21:23:26 +0100

    commit with gacp

--- IRC/classes/server.cpp
+++ IRC/classes/server.cpp
@@ -10,3 +10,20 @@ Server::~Server()
 {
     std::cout << "The Server is shutting down ... " << std::endl;
 }
+
+void	Server::startServer()
+{
+	_socket_fd = socket(_params.ai_family, _params.ai_socktype, _params.ai_protocol);
+	if (_socket_fd == -1)
+	{
+		std::cerr << "Error : The socket creation failed !" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+}
+
+void	Server::setSocketParameter()
+{
+	_params.ai_family = AF_INET;  // We choose Ipv4
+	_params.ai_socktype = SOCK_STREAM; // We choose to work with TCP stream sockets
+	_params.ai_protocol = AI_PASSIVE;
+}
