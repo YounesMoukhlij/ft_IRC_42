78daee7bd28eabbbe3221c410d0553f4e8db6526
younesmoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-28 00:57:18 +0100

    commit with gacp

--- IRC/classes/server.cpp
+++ IRC/classes/server.cpp
@@ -31,7 +31,7 @@ int Server::startServer()
         return (EXIT_FAILURE);
 	}
 
-    if (bind(_socket_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1)
+    // if (bind(_socket_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1)
     if (bind(_socket_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1)
     {
         std::cerr << "Error: Socket bind failed!" << std::endl;
