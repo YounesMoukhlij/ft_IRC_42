3d50f9d96a8de4e5ab37c82a3649c5ba36daf0b3
younesmoukhlij <younesmoukhlijofficial@gmail.com>
2024-11-28 21:33:56 +0100

    commit with gacp

--- IRC/classes/server.cpp
+++ IRC/classes/server.cpp
@@ -66,9 +66,9 @@ int Server::startServer()
 
 void	Server::setSocketParameter()
 {
-	// _params.ai_family = AF_INET;  // We choose Ipv4
-	// _params.ai_socktype = SOCK_STREAM; // We choose to work with TCP stream sockets
-	// _params.ai_protocol = AI_PASSIVE; // We choose to work with TCP stream sockets
+	_params.ai_family = AF_INET;  // We choose Ipv4
+	_params.ai_socktype = SOCK_STREAM; // We choose to work with TCP stream sockets
+	_params.ai_protocol = AI_PASSIVE; // We choose to work with TCP stream sockets
 }
 
 const char *Server::server::what(void) const throw()
