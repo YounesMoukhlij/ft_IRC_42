cf4db48f3250c1797a488b3b02bee67ffb838614
younesmoukhlij <younesmoukhlijofficial@gmail.com>
2025-01-25 22:45:41 +0100

    commit with gacp

--- IRC/classes/server.cpp
+++ IRC/classes/server.cpp
@@ -64,8 +64,6 @@ void	Server::ServerConnection()
     pollArray.push_back(server_poll);
 
 
-    printf("%d\n", pollArray[0].fd);
-    printf("%d\n", pollArray[0].events);
 	while (_socket_fd != -1)
 	{
         _poll_fd = poll(&pollArray[0], pollArray.size(), -1);
@@ -73,16 +71,14 @@ void	Server::ServerConnection()
             throw (std::logic_error("Error : The poll failed !"));
         std::cout << " -> " << _poll_fd << std::endl ;
 
-
         if (pollArray[0].revents & POLLIN)
         {
-		    std::cout << "The server is waiting for a connection ..." << std::endl;
             _client_fd = accept(_socket_fd, 0x0, 0x0);
 		    if (_client_fd == -1)
 		    	throw (std::logic_error("Error : The accept failed !"));
             std::cout << " ~~~ BOYAAH Client connected! ~~~" << std::endl;
 
-                    send(pollArray[0].fd, "buf", 4, 0);
+            send(pollArray[0].fd, "buf", 4, 0);
         }
     }
 
