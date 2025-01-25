03098f9fe3ecf73072c7e8d118e1af74a93a827b
younesmoukhlij <younesmoukhlijofficial@gmail.com>
2025-01-25 22:03:19 +0100

    commit with gacp

--- IRC/classes/server.cpp
+++ IRC/classes/server.cpp
@@ -63,7 +63,8 @@ void	Server::ServerConnection()
 	while (_socket_fd != -1)
 	{
         _poll_fd = poll(&pollArray[0], pollArray.size(), -1);
-        if ()
+        if (_poll_fd == -1)
+            throw (std::logic_error("Error : The poll failed !"));
         std::cout << " -> " << _poll_fd << std::endl ;
 
 
