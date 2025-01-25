2aa1eb1959674d1cde4b450fe931eca68a2433d9
younesmoukhlij <younesmoukhlijofficial@gmail.com>
2025-01-25 20:24:10 +0100

    commit with gacp

--- IRC/irc.cpp
+++ IRC/irc.cpp
@@ -16,14 +16,11 @@ int    InputParsing(int argc, char **argv)
     try
     {
         if (argc != 3)
-    {
-        throw std::logic_error("Error : The program started like this ./ircserv port password !" << std::endl;
-        return (EXIT_FAILURE);
-    }
+            throw std::logic_error("Error : The program started like this ./ircserv port password !\n");
         if (std::atoi(argv[1]) < 1024 || std::atoi(argv[1]) > 65535)
-            throw std::logic_error("Error : The port number must be between 1024 and 65535 !");
+            throw std::logic_error("Error : The port number must be between 1024 and 65535 !\n");
         if (strlen(argv[1]) < 4 || strlen(argv[1]) > 5)
-            throw std::logic_error("Error : The port number must be between 4 and 5 digits !");
+            throw std::logic_error("Error : The port number must be between 4 and 5 digits !\n");
     }
     catch(const std::exception& e)
     {
