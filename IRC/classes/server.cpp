c57ee235b624449d0cd8f65d5e0cfa1b45252b33
younesmoukhlij <younesmoukhlijofficial@gmail.com>
2025-01-25 22:47:05 +0100

    commit with gacp

--- IRC/classes/server.cpp
+++ IRC/classes/server.cpp
@@ -52,39 +52,120 @@ void	Server::setSocketParameter()
 
 
 
-void	Server::ServerConnection()
+// void	Server::ServerConnection()
+// {
+
+//     // char            buffer[1024];
+
+//     puts("DEBUG HERE");
+//     struct pollfd server_poll;
+//     server_poll.fd = _socket_fd;
+//     server_poll.events = POLLIN;
+//     pollArray.push_back(server_poll);
+
+
+// 	while (_socket_fd != -1)
+// 	{
+//         _poll_fd = poll(&pollArray[0], pollArray.size(), -1);
+//         if (_poll_fd == -1)
+//             throw (std::logic_error("Error : The poll failed !"));
+//         std::cout << " -> " << _poll_fd << std::endl ;
+
+//         if (pollArray[0].revents & POLLIN)
+//         {
+//             _client_fd = accept(_socket_fd, 0x0, 0x0);
+// 		    if (_client_fd == -1)
+// 		    	throw (std::logic_error("Error : The accept failed !"));
+//             std::cout << " ~~~ BOYAAH Client connected! ~~~" << std::endl;
+
+//             send(pollArray[0].fd, "buf", 4, 0);
+//         }
+//     }
+
+//     close(_client_fd);
+
+// 	}
+
+
+void Server::ServerConnection()
 {
+    char buffer[1024];
 
-    // char            buffer[1024];
-
-    puts("DEBUG HERE");
+    // Initialize pollArray to include the server socket
     struct pollfd server_poll;
-    server_poll.fd = _socket_fd;
-    server_poll.events = POLLIN; 
-    pollArray.push_back(server_poll);
+    server_poll.fd = _socket_fd;  // Server socket for listening
+    server_poll.events = POLLIN;  // We want to listen for input events
+    pollArray.push_back(server_poll);  // Add server socket to pollArray
 
+    while (_socket_fd != -1)
+    {
+        // Wait for events
+        int poll_fd = poll(&pollArray[0], pollArray.size(), -1);
+        if (poll_fd == -1)
+        {
+            std::cerr << "Error: The poll failed!" << std::endl;
+            return;  // Handle error and exit the function
+        }
 
-	while (_socket_fd != -1)
-	{
-        _poll_fd = poll(&pollArray[0], pollArray.size(), -1);
-        if (_poll_fd == -1)
-            throw (std::logic_error("Error : The poll failed !"));
-        std::cout << " -> " << _poll_fd << std::endl ;
+        std::cout << " -> " << poll_fd << std::endl;
 
+        // Check if the server socket is ready (new connection)
         if (pollArray[0].revents & POLLIN)
         {
-            _client_fd = accept(_socket_fd, 0x0, 0x0);
-		    if (_client_fd == -1)
-		    	throw (std::logic_error("Error : The accept failed !"));
-            std::cout << " ~~~ BOYAAH Client connected! ~~~" << std::endl;
+            std::cout << "The server is waiting for a connection ..." << std::endl;
+            _client_fd = accept(_socket_fd, nullptr, nullptr);
+            if (_client_fd == -1)
+            {
+                std::cerr << "Error: The accept failed!" << std::endl;
+                return;  // Handle error and exit the function
+            }
 
-            send(pollArray[0].fd, "buf", 4, 0);
+            std::cout << "Connection Established." << std::endl;
+
+            // Add the client socket to the poll array to read data from it
+            struct pollfd client_poll;
+            client_poll.fd = _client_fd;
+            client_poll.events = POLLIN;
+            pollArray.push_back(client_poll);  // Add client to poll array
+        }
+
+        // Now check all client sockets for data
+        for (size_t i = 1; i < pollArray.size(); ++i)  // Start from 1 to skip server socket
+        {
+            if (pollArray[i].fd < 0)  // Ensure client socket is valid
+            {
+                std::cerr << "Invalid client socket detected at index " << i << std::endl;
+                continue;  // Skip invalid sockets
+            }
+
+            // If the client socket has data to read
+            if (pollArray[i].revents & POLLIN)
+            {
+                ssize_t bytes_received = recv(pollArray[i].fd, buffer, sizeof(buffer) - 1, 0);
+                if (bytes_received > 0)
+                {
+                    buffer[bytes_received] = '\0';  // Null-terminate the received message
+                    std::cout << "Received from client: " << buffer << std::endl;
+
+                    // Echo the message back to the client
+                    send(pollArray[i].fd, buffer, bytes_received, 0);
+                }
+                else if (bytes_received == 0)
+                {
+                    std::cout << "Client disconnected." << std::endl;
+                    close(pollArray[i].fd);  // Close the socket
+                    pollArray.erase(pollArray.begin() + i);  // Remove client from poll array
+                    --i;  // Adjust index after removal
+                }
+                else
+                {
+                    std::cerr << "Error receiving data from client." << std::endl;
+                }
+            }
         }
     }
+}
 
-    close(_client_fd);
-
-	}
 
 
 void	Server::ShutServer()
