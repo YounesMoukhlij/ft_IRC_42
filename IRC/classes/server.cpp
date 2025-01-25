1e314177b96820d33a797517563e306aedbe967f
younesmoukhlij <younesmoukhlijofficial@gmail.com>
2025-01-25 22:16:50 +0100

    commit with gacp

--- IRC/classes/server.cpp
+++ IRC/classes/server.cpp
@@ -55,46 +55,71 @@ void	Server::setSocketParameter()
 
 
 
-void	Server::ServerConnection()
+void Server::ServerConnection()
 {
+    char buffer[1024];
 
-    char    buffer[1024];
+    // Poll setup for server socket first
+    pollArray[0].fd = _socket_fd;
+    pollArray[0].events = POLLIN;
 
-	while (_socket_fd != -1)
-	{
-        _poll_fd = poll(&pollArray[0], pollArray.size(), -1);
-        if (_poll_fd == -1)
-            throw (std::logic_error("Error : The poll failed !"));
-        std::cout << " -> " << _poll_fd << std::endl ;
+    while (_socket_fd != -1)
+    {
+        // Wait for events on any of the sockets
+        int poll_fd = poll(&pollArray[0], pollArray.size(), -1);
+        if (poll_fd == -1)
+            throw std::logic_error("Error: The poll failed!");
 
+        std::cout << " -> " << poll_fd << std::endl;
 
-        pollArray[0].fd = _socket_fd ;
-        pollArray[0].events = POLLIN ;
-		std::cout << "The server is waiting for a connection ..." << std::endl;
-        _client_fd = accept(_socket_fd, 0x0, 0x0);
-		if (_client_fd == -1)
-			throw (std::logic_error("Error : The accept failed !"));
-        std::cout << "Connection Established." << std::endl;
-
-        // Read data from the client
-        ssize_t bytes_received;
-        while ((bytes_received = recv(_client_fd, buffer, sizeof(buffer) - 1, 0)) > 0)
+        // Check if the server socket is ready (new connection)
+        if (pollArray[0].revents & POLLIN)
         {
-            buffer[bytes_received] = '\0';  // Null-terminate the string
-            
-            std::cout << "Received from client: " << buffer << std::endl;
-            // Echo the message back to the client (optional)
-            send(_client_fd, buffer, bytes_received, 0);
-        }
-        if (bytes_received == 0) {
-            std::cout << "Client disconnected." << std::endl;
-        } else if (bytes_received == -1) {
-            std::cerr << "Error receiving data from client." << std::endl;
-        }
-        close(_client_fd);
+            std::cout << "The server is waiting for a connection ..." << std::endl;
+            _client_fd = accept(_socket_fd, nullptr, nullptr);
+            if (_client_fd == -1)
+                throw std::logic_error("Error: The accept failed!");
 
-	}
+            std::cout << "Connection Established." << std::endl;
+
+            // Add the client socket to the poll array to read data from it
+            struct pollfd client_poll;
+            client_poll.fd = _client_fd;
+            client_poll.events = POLLIN;
+            pollArray.push_back(client_poll);  // Add client to poll array
+        }
+
+        // Now check all client sockets for data
+        for (size_t i = 1; i < pollArray.size(); ++i)
+        {
+            // If the client socket has data to read
+            if (pollArray[i].revents & POLLIN)
+            {
+                ssize_t bytes_received = recv(pollArray[i].fd, buffer, sizeof(buffer) - 1, 0);
+                if (bytes_received > 0)
+                {
+                    buffer[bytes_received] = '\0';  // Null-terminate the received message
+                    std::cout << "Received from client: " << buffer << std::endl;
+
+                    // Echo the message back to the client
+                    send(pollArray[i].fd, buffer, bytes_received, 0);
+                }
+                else if (bytes_received == 0)
+                {
+                    std::cout << "Client disconnected." << std::endl;
+                    close(pollArray[i].fd);  // Close the socket
+                    pollArray.erase(pollArray.begin() + i);  // Remove client from poll array
+                    --i;  // Adjust index after removal
+                }
+                else
+                {
+                    std::cerr << "Error receiving data from client." << std::endl;
+                }
+            }
+        }
+    }
 }
+
 
 void	Server::ShutServer()
 {
